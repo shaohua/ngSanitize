be20c5b43d691bd69e1955b16561a192e442ca09
Shaohua Zhou <shao@versal.com>
2014-03-14 12:06:11 -0700

    More tests, some are failing

--- test/basic_functions_spec.js
+++ test/basic_functions_spec.js
@@ -18,5 +18,36 @@ function(NgSanitize){
       expect(output).to.equal('<p>an html&#10;<em>click here</em>&#10;snippet</p>');
     });
 
+    it('should NOT remove image tag', function(){
+      var input = "test<img src=' http://static.jquery.com/files/rocker/images/logo_jquery_215x53.gif' onload='alert(\"hi there\")'>";
+      var output = ngSanitize(input);
+      expect(output).to.equal(input);
+    });
+
+    it('should remove script tag', function(){
+      var input = "test<script>alert('Hi!'');</script>";
+      var output = ngSanitize(input);
+      expect(output).to.equal('test');
+    });
+
+    it('should remove malformatted a tag version 1', function(){
+      var input = "test<a href=javascript:(alert('oh no'))>CLICK HERE FOR FREE CAR!</a>";
+      var output = ngSanitize(input);
+      expect(output).to.equal('test<a>CLICK HERE FOR FREE CAR!</a>');
+    });
+
+    it('should remove malformatted a tag version 2', function(){
+      var input = "test<a href='javascript:(alert(1))'>CLICK HERE FOR FREE CAR!</a>";
+      var output = ngSanitize(input);
+      expect(output).to.equal('test<a>CLICK HERE FOR FREE CAR!</a>');
+    });
+
+    it('should remove incomplete a tag', function(){
+      var input = 'test<a href=">" onmouseover="attackCode()">';
+      var output = ngSanitize(input);
+      expect(output).to.equal('test<a></a>');
+    });
+
+
   });
 });
